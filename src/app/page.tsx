e963283b6da357041082d758c039a19a39b29817
mpses <nsorangepv@gmail.com>
2024-01-01 23:11:58 +0900

    [update] Create top page which resolves #11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,10 +35,67 @@ export default function Home() {
     },
   ];
   return (
-    <div className="">
-      <div className="flex items-center">
-        <h1 className="font-bold text-2xl">記事一覧</h1>
-        
+    <div>
+      <h1 className="font-bold text-3xl mb-8">記事一覧</h1>
+      <div className="flex justify-between">
+        {Object.keys(categories).map((key) => {
+          const subCategories: string[] = Array.from(
+            new Map(
+              testData
+                .filter((e) => e.category === categories[key])
+                .map((e) => [e.subCategory, e.subCategory])
+            ),
+            ([, v]) => v
+          );
+
+          return (
+            <div className="mr-4 w-[24vw]" key={key}>
+              <a className="font-semibold text-2xl mb-6" href={`#${key}`}>
+                {categories[key]}
+              </a>
+              <div>
+                <div className="mb-[-24px]">
+                  <a className="text-base mb-10" href={`#${key}`}>
+                    #{key}
+                  </a>
+                </div>
+                {subCategories.map((subCategory) => {
+                  return (
+                    <div className="" key={subCategory}>
+                      <div className="font-semibold text-xl mt-16 mb-4">
+                        {subCategory}
+                      </div>
+                      <div>
+                        {testData
+                          .filter(
+                            (e) =>
+                              e.category === categories[key] &&
+                              e.subCategory === subCategory
+                          )
+                          .map((e) => {
+                            return (
+                              <div
+                                className="w-[20vw] border rounded p-4 mb-4"
+                                key={e.title}
+                              >
+                                <a
+                                  className="font-semibold text-lg mb-4"
+                                  href={`/${e.title}/edit`}
+                                >
+                                  {e.title}
+                                </a>
+                                <div className="text-sm mt-2">最終更新 {e.date}</div>
+                              </div>
+                            );
+                          })}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
