34f81042c107082783c9b6caaa2ea4f952c222e4
ryota-sale <mitani-ryota@g.ecc.u-tokyo.ac.jp>
2024-01-06 10:48:57 +0900

    [fix] close #44

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   // console.log(articlesData); //ここに新規で送信した記事が出てこない
 
   return (
-    <div>
+    <div className="pb-24">
       <h1 className="font-bold text-3xl mb-8">記事一覧</h1>
       <ArticleLists articlesData={articlesData} />
     </div>
