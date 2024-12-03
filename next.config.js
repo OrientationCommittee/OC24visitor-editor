8ca0a240cb636480f61f696108b9743a3888667b
23micchan <mitani-ryota@g.ecc.u-tokyo.ac.jp>
2024-12-04 08:38:42 +0900

    [update] Change basePath: /24/editor -> /editor

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_APP_MODE === "staging" ? "/24/editor-stg" : "/24/editor";
+const basePath = process.env.NEXT_PUBLIC_APP_MODE === "staging" ? "/editor-stg" : "/editor";
 
 const nextConfig = {
   basePath,
